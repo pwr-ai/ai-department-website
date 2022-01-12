8f4c42d0a774725819c1d949a731bf32959346d2
Marcin Wątroba <markowanga@gmail.com>
2022-01-12 10:59:11 +0100

    Add date fix

--- .github/publication_processor/add_missing_publications.py
+++ .github/publication_processor/add_missing_publications.py
@@ -28,6 +28,10 @@ def get_bib_path(pub_scrapped_id: str) -> str:
     return f'scholar/scholar_publication/{pub_scrapped_id}.bib'
 
 
+def get_json_path(pub_scrapped_id: str) -> str:
+    return f'scholar/scholar_publication/{pub_scrapped_id}.json'
+
+
 def add_pub_with_id(pub_scrapped_id: str):
     print(f'adding publication {pub_scrapped_id}')
     bib_path = get_bib_path(pub_scrapped_id)
@@ -38,5 +42,11 @@ def add_pub_with_id(pub_scrapped_id: str):
 
 
 if __name__ == '__main__':
-    for pub_id in get_scrapped_uid():
-        add_pub_with_id(pub_id)
+    all_pubs = json.load(open('scholar/pubs.json', 'r'))
+    for pub_id in get_scrapped_uid()[:2]:
+        title = json.load(open(get_json_path(pub_id), 'r'))['bib']['title']
+        if title in all_pubs:
+            add_pub_with_id(pub_id)
+    # for pub_id in get_scrapped_uid()[:10]:
+    #     title = json.load(open(get_json_path(pub_id), 'r'))['bib']['title']
+    #     print(title in all_pubs, title)
